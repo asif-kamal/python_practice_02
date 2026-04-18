fd79f17843eb76706d87e1c265e725a8f3730219
asif-kamal <asif.kamal1489@gmail.com>
2026-04-18 11:05:58 -0500

    tip calculator

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello"[-1])
+print("Welcome to the tip calculator")
+cost = input("Enter cost of service: ")
+tip = float(cost) * 0.15
+print("Your tip amount is:", round(tip, 2))
+print(f"The total cost is {round(tip + float(cost), 2)}")
