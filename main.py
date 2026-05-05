47c2bd45bb003540876e51cdfa03c4004f858023
asif-kamal <asif.kamal1489@gmail.com>
2026-05-05 14:56:13 -0500

    added Student class

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import os
-
-print("Welcome to bidding auction!")
-dict = {}
-maximum = 0
-highest_bidder = ""
-answer = "y"
-# name = input("Please enter your name: ")
-# dict[name] = float(input("Please enter your bid: "))
+# import os
 #
-# answer = input("Are there other bidders? y/N ").lower()
+# print("Welcome to bidding auction!")
+# dict = {}
+# maximum = 0
+# highest_bidder = ""
+# answer = "y"
+# # name = input("Please enter your name: ")
+# # dict[name] = float(input("Please enter your bid: "))
+# #
+# # answer = input("Are there other bidders? y/N ").lower()
+#
+# while answer != "n":
+#     name = input("Please enter your name: ")
+#     dict[name] = float(input("Please enter your bid: "))
+#     answer = input("Are there other bidders? y/N ").lower()
+#
+#     print("\n" * 50)
+#
+#
+# for entry in dict.keys():
+#     if dict[entry] > maximum:
+#         maximum = dict[entry]
+#         highest_bidder = entry
+#
+#
+#
+#
+# print(f"The highest bidder with ${round(maximum, 2)} is {highest_bidder}!")
 
-while answer != "n":
-    name = input("Please enter your name: ")
-    dict[name] = float(input("Please enter your bid: "))
-    answer = input("Are there other bidders? y/N ").lower()
+from student import Student
 
-    print("\n" * 50)
+def main():
+    Student(input("What is your name? "), input("What is your house? "))
 
-
-for entry in dict.keys():
-    if dict[entry] > maximum:
-        maximum = dict[entry]
-        highest_bidder = entry
-
-
-
-
-print(f"The highest bidder with ${round(maximum, 2)} is {highest_bidder}!")
+if __name__ == "__main__":
+    main()
---
+++ student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+        print(f"{self.name} is in {self.house}")
+
